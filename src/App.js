6a8daf396bd77232db9e2bf17bd45e646b19ad06
Carla Vieira <carlaamvieira@gmail.com>
2019-11-23 08:29:33 -0300

    change route product

--- src/App.js
+++ src/App.js
@@ -1,68 +1,67 @@
-import React, { Component } from 'react';
-import './App.scss';
-import {Router,Route ,Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.scss";
+import { Router, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
-import { setProducts } from './js/actions/index'
-import Header from './js/components/Header';
-import axios from 'axios';
-import HomePage from './js/pages/HomePage';
-import ProductPage from './js/pages/ProductPage';
-import SearchPage from './js/pages/SearchPage'
+import { setProducts } from "./js/actions/index";
+import Header from "./js/components/Header";
+import axios from "axios";
+import HomePage from "./js/pages/HomePage";
+import ProductPage from "./js/pages/ProductPage";
+import SearchPage from "./js/pages/SearchPage";
 
-
-import history from './history';
-
-
-
+import history from "./history";
 
 const mapStateToProps = state => {
-  return { products: state.products,searchElement:state.searchElement};
+  return { products: state.products, searchElement: state.searchElement };
 };
-const mapDispatchToProps = (dispatch) =>({
-  setProducts: (products) => dispatch(setProducts(products))
+const mapDispatchToProps = dispatch => ({
+  setProducts: products => dispatch(setProducts(products))
 });
 export class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-        products:{data:[]
-      },
-      loading:true
+      products: { data: [] },
+      loading: true
+    };
+    this._setState();
+  }
+  async _setState() {
+    let response = [];
+    try {
+      response = await axios.get(
+        "https://cors-anywhere.herokuapp.com/https://funil-mock.herokuapp.com/example1 "
+      );
+    } catch (error) {
+      console.log("Error ", error);
     }
-    this._setState()
-  }
-  async _setState(){
-    let response=[]
-    try{
-      response = await axios.get('https://cors-anywhere.herokuapp.com/https://funil-mock.herokuapp.com/example1 ')
-    } catch(error){
-      console.log("Error ",error)
-    }  
-    this.props.setProducts(response.data.data)
+    this.props.setProducts(response.data.data);
     //Passar o state de loading para a store do redux depois
-    this.setState({loading:false})
+    this.setState({ loading: false });
   }
-  
+
   render() {
-    const {loading} = this.state
-      return (
-        <Router history ={history}>
-          <div className="App">
-              <Header></Header>
-              {/* implementar uma loading screen futuramente */}
-              {loading ? "Segura ae" : 
-              <div>
+    const { loading } = this.state;
+    return (
+      <Router history={history}>
+        <div className="App">
+          <Header></Header>
+          {/* implementar uma loading screen futuramente */}
+          {loading ? (
+            "Segura ae"
+          ) : (
+            <div>
               <Switch>
-                <Route exact path="/" component={HomePage}/>
-                <Route exact path="/:id" component={ProductPage}/>
-                <Route path="/search/" component={SearchPage}/>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/product/:id" component={ProductPage} />
+                <Route path="/search/" component={SearchPage} />
               </Switch>
-              </div>
-              }
-          </div>
-        </Router>
-      )
+            </div>
+          )}
+        </div>
+      </Router>
+    );
   }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -1,31 +1,44 @@
-import React, { Component } from 'react'
-import { Card as CardBoot , Button} from 'react-bootstrap';
-import {Link} from 'react-router-dom'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React, { Component } from "react";
+import { Card as CardBoot, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 export class Card extends Component {
-    render() {
-        const {image,name,brand,category,currency,price,color,id} = this.props.product
-        return (
-            <CardBoot style={{ width: '18rem' }}>
-                <Link to={`/${id}`}><CardBoot.Img variant="top" src={image} /></Link>
-                <CardBoot.Body>
-                 <Link  to={`/${id}`}>{name}</Link>
-                    <CardBoot.Text>
-                        Categoria: {category}
-                        <br/>
-                        Marca: {brand}
-                        <br/>
-                        {/* INTL NUMBER FORMAT */}
-                        {currency}: {price}
-                        <canvas width="20px" height="20px" style={{marginLeft:'20px',backgroundColor:color}}>
-
-                        </canvas>
-                    </CardBoot.Text>
-                </CardBoot.Body>
-            </CardBoot>
-        )
-    }
+  render() {
+    const {
+      image,
+      name,
+      brand,
+      category,
+      currency,
+      price,
+      color,
+      id
+    } = this.props.product;
+    return (
+      <CardBoot style={{ width: "18rem" }}>
+        <Link to={`/product/${id}`}>
+          <CardBoot.Img variant="top" src={image} />
+        </Link>
+        <CardBoot.Body>
+          <Link to={`/product/${id}`}>{name}</Link>
+          <CardBoot.Text>
+            Categoria: {category}
+            <br />
+            Marca: {brand}
+            <br />
+            {/* INTL NUMBER FORMAT */}
+            {currency}: {price}
+            <canvas
+              width="20px"
+              height="20px"
+              style={{ marginLeft: "20px", backgroundColor: color }}
+            ></canvas>
+          </CardBoot.Text>
+        </CardBoot.Body>
+      </CardBoot>
+    );
+  }
 }
 
-export default Card
+export default Card;
