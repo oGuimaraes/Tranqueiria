482eb0323f342d7c3614946bf7dda66e52ae2f77
oGuimaraes <otaviovgsr@gmail.com>
2019-12-05 10:33:29 -0300

    fix: card-styling bug fix

--- src/js/pages/CategoryPage.js
+++ src/js/pages/CategoryPage.js
@@ -14,6 +14,7 @@ const mapStateToProps = state => {
         sort:state.sort
     };
 };
+
 const mapDispatchToProps = (dispatch) =>({
     setSort: (sortOption) => dispatch(setSort(sortOption))
 });
