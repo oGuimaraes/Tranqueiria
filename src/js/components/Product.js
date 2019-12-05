97c13929f8cdf1110f991b9aa1f4b363a2d958bd
oGuimaraes <otaviovgsr@gmail.com>
2019-12-05 02:18:24 -0300

    product-card v.mobile styling

--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -16,33 +16,58 @@ export class Product extends Component {
     render() {
         const {image,name,brand,category,currency,price,color,id} = this.props.product
         return (
-            <CardBoot className="product-card" style={{ width: '56rem' }}>
-                <div className="row">
-                <div className="col-md-6">
-                <CardBoot.Img className="product-image" variant="top" style={{ width: '28rem' }} src={image} />
-                </div>
-                <div className="col-md-6">
-                <CardBoot.Body>
-                <CardBoot.Title className="product-title">{name}</CardBoot.Title>
-                    <CardBoot.Text>
-                        <span class="key-text">Categoria</span> {category}
-                        <br/>
-                        <span class="key-text">Marca:</span> {brand}
-                        <br/>
-                        {/* INTL NUMBER FORMAT */}
-                        <span class="key-text">R$:</span> {price}
-                        <br/>
-                        <span class="key-text">Cor Primária:</span> <canvas width="20px" height="20px" style={{marginLeft:'20px',backgroundColor:color}}>
-
-                        </canvas>
-                    </CardBoot.Text>
-                    <div className="button-section">
-                        <Button variant="primary" onClick={this.handleClickAddToCart} >Adicionar ao Carrinho</Button>
+            <div>
+                <CardBoot className="product-card" style={{ width: '56rem' }}>
+                    <div className="row">
+                    <div className="col-md-6">
+                    <CardBoot.Img className="product-image" variant="top" style={{ width: '28rem' }} src={image} />
                     </div>
-                </CardBoot.Body>
+                    <div className="col-md-6">
+                    <CardBoot.Body>
+                    <CardBoot.Title className="product-title">{name}</CardBoot.Title>
+                        <CardBoot.Text>
+                            <span class="key-text">Categoria</span> {category}
+                            <br/>
+                            <span class="key-text">Marca:</span> {brand}
+                            <br/>
+                            {/* INTL NUMBER FORMAT */}
+                            <span class="key-text">R$:</span> {price}
+                            <br/>
+                            <span class="key-text">Cor Primária:</span> <canvas width="20px" height="20px" style={{marginLeft:'20px',backgroundColor:color}}>
+
+                            </canvas>
+                        </CardBoot.Text>
+                        <div className="button-section">
+                            <Button variant="primary" onClick={this.handleClickAddToCart} >Adicionar ao Carrinho</Button>
+                        </div>
+
+                    </CardBoot.Body>
+                    </div>
+                    </div>
+                </CardBoot>
+                <div className="product-card-mobile">
+                    <CardBoot.Img className="product-image" variant="top" src={image}/>
+                    <CardBoot.Body>
+                    <CardBoot.Title className="product-title">{name}</CardBoot.Title>
+                        <CardBoot.Text>
+                            <span class="key-text">Categoria</span> {category}
+                            <br/>
+                            <span class="key-text">Marca:</span> {brand}
+                            <br/>
+                            {/* INTL NUMBER FORMAT */}
+                            <span class="key-text">R$:</span> {price}
+                            <br/>
+                            <span class="key-text">Cor Primária:</span> <canvas width="20px" height="20px" style={{marginLeft:'20px',backgroundColor:color}}>
+
+                            </canvas>
+                        </CardBoot.Text>
+                        <div className="button-section">
+                            <Button variant="primary" onClick={this.handleClickAddToCart} >Adicionar ao Carrinho</Button>
+                        </div>
+
+                    </CardBoot.Body>
                 </div>
-                </div>
-            </CardBoot>
+            </div>
         )
     }
 }
