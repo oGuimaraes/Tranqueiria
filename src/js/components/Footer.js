4d6bc236bec699c9198a78d187a972ad5346efee
Carla Vieira <carlaamvieira@gmail.com>
2019-12-05 09:55:43 -0300

    fix: imagens git/linkedin footer

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -1,60 +1,71 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import Logo from '../../img/logo.svg'
-import Github from '../../img/github.png'
-import LinkedIn from '../../img/linkedin.png'
-import Mail from '../../img/mail.png'
-import '../../sass/Footer.scss'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../img/logo.svg";
+import Github from "../../img/github.png";
+import LinkedIn from "../../img/linkedin.png";
+import Mail from "../../img/mail.png";
+import "../../sass/Footer.scss";
 
 export default class Footer extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            grupo:[
-            {
-                nome: "Carla D'Abreu",
-                linkedin: "aaa",
-                github: "aaa",
-                email: "aaa",
-            }, 
-            {
-                nome: "Gabriel Lopes",
-                linkedin: "",
-                github: "",
-                email: "",
-            },
-            {
-                nome: "Luiz Gustavo Torres",
-                linkedin: "",
-                github: "",
-                email: "",
-            },
-            {
-                nome: "Otávio Guimarães",
-                linkedin: "",
-                github: "",
-                email: "",
-            }
-          ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      grupo: [
+        {
+          nome: "Carla D'Abreu",
+          linkedin: "/carladabreu",
+          github: "/carlavieira",
+          email: "carlaamvieira@gmail.com"
+        },
+        {
+          nome: "Gabriel Lopes",
+          linkedin: "",
+          github: "",
+          email: ""
+        },
+        {
+          nome: "Luiz Gustavo Torres",
+          linkedin: "",
+          github: "",
+          email: ""
+        },
+        {
+          nome: "Otávio Guimarães",
+          linkedin: "",
+          github: "",
+          email: ""
         }
-    }
+      ]
+    };
+  }
 
-    render() {
-        return (
-            <div id="footer">
-                <Link to="/"><img src={Logo} id="logoFooter" /></Link>
-                <p id="nomeEmpresa">© 2019 - Tranqueiria</p>
-                {this.state.grupo.map(elemento =>
-                        <p>
-                            <ul className="nomeTime">
-                                <li>{elemento.nome}</li>
-                                <li><img src={Github} className="imagensFooter"/>{elemento.linkedin}</li>
-                                <li><img src={LinkedIn} className="imagensFooter"/>{elemento.github}</li>
-                                <li><img src={Mail} className="imagensFooter"/>{elemento.email}</li>
-                            </ul>
-                        </p>
-                )}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="footer">
+        <Link to="/">
+          <img src={Logo} id="logoFooter" />
+        </Link>
+        <p id="nomeEmpresa">© 2019 - Tranqueiria</p>
+        {this.state.grupo.map(elemento => (
+          <p>
+            <ul className="nomeTime">
+              <li>{elemento.nome}</li>
+              <li>
+                <img src={Github} className="imagensFooter" />
+                {elemento.github}
+              </li>
+              <li>
+                <img src={LinkedIn} className="imagensFooter" />
+                {elemento.linkedin}
+              </li>
+              <li>
+                <img src={Mail} className="imagensFooter" />
+                {elemento.email}
+              </li>
+            </ul>
+          </p>
+        ))}
+      </div>
+    );
+  }
 }
